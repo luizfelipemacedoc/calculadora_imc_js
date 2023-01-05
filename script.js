fbb9af7c3e245e009d63f0cd7eb25fe13e7db46b
Luiz Macedo <felipemacedoc89@gmail.com>
2023-01-05 16:32:26 -0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let btn = document.querySelector('#botão');
+btn.addEventListener('click', calcular);
 //função para a o botão calcular
 function calcular() {
     //pegar inputs do usuário
-    const input_altura = document.getElementById('altura').value;
-    const input_peso = document.getElementById('peso').value;
+    const input_altura = document.querySelector('#altura').value;
+    const input_peso = document.querySelector('#peso').value;
     //formula imc
     let imc = parseFloat(input_peso / (input_altura * input_altura)).toFixed(2);
     //condicional para exibir resultado
@@ -10,15 +12,15 @@ function calcular() {
       alert("Não podem haver campos vazios!")
     }
     else if (imc < 18.5) {
-      document.getElementById('resultado').innerHTML = `Seu IMC é de ${imc}kg/m². <br> Você está abaixo do peso.`;
+      document.querySelector('#resultado').innerHTML = `Seu IMC é de ${imc}kg/m². <br> Você está abaixo do peso.`;
     } 
     else if (imc >= 18.5 && imc <= 24.9) {
-      document.getElementById('resultado').innerHTML = `Seu IMC é de ${imc}kg/m². <br> Você está com peso normal.`;
+      document.querySelector('#resultado').innerHTML = `Seu IMC é de ${imc}kg/m². <br> Você está com peso normal.`;
     } 
     else if (imc >= 25 && imc <= 29.9) {
-      document.getElementById('resultado').innerHTML = `Seu IMC é de ${imc}kg/m². <br> Você está com sobrepeso.`;
+      document.querySelector('#resultado').innerHTML = `Seu IMC é de ${imc}kg/m². <br> Você está com sobrepeso.`;
     } 
     else if (imc > 30) {
-      document.getElementById('resultado').innerHTML = `Seu IMC é de ${imc}kg/m². <br> Você está obeso.`;
+      document.querySelector('#resultado').innerHTML = `Seu IMC é de ${imc}kg/m². <br> Você está obeso.`;
     }
   }
